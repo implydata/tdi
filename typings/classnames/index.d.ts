f3374c66cd7ea068752b6156bdcbb236c16ff1d4
Vadim Ogievetsky <vadimon@gmail.com>
2016-12-02 23:14:26 -0800

    add classnames

---
+++ typings/classnames/index.d.ts
@@ -0,0 +1,5 @@
+declare module "classnames" {
+    function classnames(...parts: any[]): string;
+    namespace classnames {};
+    export = classnames;
+}
--- typings/has-own-prop/index.d.ts
+++ typings/has-own-prop/index.d.ts
@@ -1,5 +1,5 @@
 declare module "has-own-prop" {
     function hop(object: any, key: string): boolean;
+    namespace hop {};
     export = hop;
 }
-
