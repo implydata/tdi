69fd7c09e0dd5a5cee83e033b110966e9800b229
Vadim Ogievetsky <vadimon@gmail.com>
2016-12-06 16:26:43 -0800

    small fix

--- typings/has-own-prop/index.d.ts
+++ typings/has-own-prop/index.d.ts
@@ -1,5 +1,5 @@
 declare module "has-own-prop" {
-    function hop(object: any, key: string): boolean;
+    function hop(object: any, key: string | number): boolean;
     namespace hop {};
     export = hop;
 }
