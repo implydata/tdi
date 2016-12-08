e711d7c30ae315b84d57339faed8a32f1c1c3284
eva <evaliou@imply.io>
2016-12-07 18:58:05 -0800

    actual types

--- typings/owasp-password-strength-test/owasp-password-strength-test.d.ts
+++ typings/owasp-password-strength-test/owasp-password-strength-test.d.ts
@@ -1,29 +1,22 @@
-export interface PassRuleOptions {
-  allowPassphrases?: boolean;
-  maxLength?: number;
-  minLength: number;
-  minPhraseLength?: number;
-  minOptionalTestsToPass?: number;
-}
-
-export interface PassTestResult {
-  errors: string[];
-  failedTests: number[];
-  passedTests: number[];
-  requiredTestErrors: string[];
-  optionalTestErrors: string[];
-  isPassphrase: boolean;
-  strong: boolean;
-  optionalTestsPassed: number;
-}
-
-interface OwaspPasswordStrengthTest {
-  config(o: PassRuleOptions): void;
-  test(s: string): PassTestResult;
-}
-
-declare var owaspPasswordStrengthTest: OwaspPasswordStrengthTest;
-
 declare module 'owasp-password-strength-test' {
-  export = owaspPasswordStrengthTest;
+  export interface PassRuleOptions {
+    allowPassphrases?: boolean;
+    maxLength?: number;
+    minLength?: number;
+    minPhraseLength?: number;
+    minOptionalTestsToPass?: number;
+  }
+
+  export interface PassTestResult {
+    errors: string[];
+    failedTests: number[];
+    passedTests: number[];
+    requiredTestErrors: string[];
+    optionalTestErrors: string[];
+    isPassphrase: boolean;
+    strong: boolean;
+    optionalTestsPassed: number;
+  }
+  export var config: (o: PassRuleOptions) => void;
+  export var test: (s: string) => PassTestResult;
 }
