4f8f874c01e8efa1ad0daae12b0ba72eba45e130
eva <evaliou@imply.io>
2016-12-07 18:51:04 -0800

    real types

--- typings/owasp-password-strength-test/owasp-password-strength-test.d.ts
+++ typings/owasp-password-strength-test/owasp-password-strength-test.d.ts
@@ -1,4 +1,29 @@
+export interface PassRuleOptions {
+  allowPassphrases?: boolean;
+  maxLength?: number;
+  minLength: number;
+  minPhraseLength?: number;
+  minOptionalTestsToPass?: number;
+}
+
+export interface PassTestResult {
+  errors: string[];
+  failedTests: number[];
+  passedTests: number[];
+  requiredTestErrors: string[];
+  optionalTestErrors: string[];
+  isPassphrase: boolean;
+  strong: boolean;
+  optionalTestsPassed: number;
+}
+
+interface OwaspPasswordStrengthTest {
+  config(o: PassRuleOptions): void;
+  test(s: string): PassTestResult;
+}
+
+declare var owaspPasswordStrengthTest: OwaspPasswordStrengthTest;
+
 declare module 'owasp-password-strength-test' {
-    var p: any;
-    export = p;
+  export = owaspPasswordStrengthTest;
 }
