c49adc7c8d25eb9b037d435f65d5b3628c71aa3c
Vadim Ogievetsky <vadimon@gmail.com>
2016-12-10 16:54:22 -0800

    updated for table 4.0.1

--- typings/table/table.d.ts
+++ typings/table/table.d.ts
@@ -1,5 +1,43 @@
 declare module "table" {
-  var p: any;
-  export function getBorderCharacters(name: string): any;
-  export default p;
+
+  export interface TableColumn {
+    alignment?: string; // Cell content alignment (enum: left, center, right) (default: left).
+    width?: number; // Column width (default: auto).
+    truncate?: number; // Number of characters are which the content will be truncated (default: Infinity).
+    paddingLeft?: number; // Cell content padding width left (default: 1).
+    paddingRight?: number; // Cell content padding width right (default: 1).
+  }
+
+  export interface TableBorder {
+    topBody?: string;
+    topJoin?: string;
+    topLeft?: string;
+    topRight?: string;
+    bottomBody?: string;
+    bottomJoin?: string;
+    bottomLeft?: string;
+    bottomRight?: string;
+    bodyLeft?: string;
+    bodyRight?: string;
+    bodyJoin?: string;
+    joinBody?: string;
+    joinLeft?: string;
+    joinRight?: string;
+    joinJoin?: string;
+  }
+
+  export interface DrawJoin {
+    (index?: number, size?: number): boolean;
+  }
+
+  export interface TableConfig {
+    border?: TableBorder;
+    columns?: TableColumn[];
+    columnDefault?: TableColumn[];
+    drawHorizontalLine: DrawJoin;
+  }
+
+  export function getBorderCharacters(name: string): TableBorder;
+  export function table(rows: any[][], config: TableConfig): string;
+  export function createStream(config: TableConfig): any;
 }
