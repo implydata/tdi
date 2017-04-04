370202d69f08c6a00185cbd28cd99ad425756676
Vadim Ogievetsky <vadimon@gmail.com>
2017-04-04 15:47:31 -0700

    fix classnames dep

--- typings/classnames/index.d.ts
+++ typings/classnames/index.d.ts
@@ -3,20 +3,20 @@
 // Definitions by: Dave Keen <http://www.keendevelopment.ch>, Adi Dahiya <https://github.com/adidahiya>, Jason Killian <https://github.com/JKillian>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare type ClassValue = string | number | ClassDictionary | ClassArray | undefined | null | false;
-
-interface ClassDictionary {
-  [id: string]: boolean | undefined | null;
-}
-
-interface ClassArray extends Array<ClassValue> { }
-
-interface ClassNamesFn {
-  (...classes: ClassValue[]): string;
-}
-
-declare var classNames: ClassNamesFn;
-
 declare module "classnames" {
+  type ClassValue = string | number | ClassDictionary | ClassArray | undefined | null | false;
+
+  interface ClassDictionary {
+    [id: string]: boolean | undefined | null;
+  }
+
+  interface ClassArray extends Array<ClassValue> { }
+
+  interface ClassNamesFn {
+    (...classes: ClassValue[]): string;
+  }
+
+  var classNames: ClassNamesFn;
+
   export = classNames
 }
